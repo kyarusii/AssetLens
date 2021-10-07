8f13a4d26defef45e78713651c29ef0e10c6e152
seonghwan-dev <Seonghwan@pm.me>
2021-10-07 12:50:40 +0900

    Check whether it is initialized or not only once.

--- Editor/Reference/Callback/ReferenceEntrypoint.cs
+++ Editor/Reference/Callback/ReferenceEntrypoint.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using UnityEditor;
+using UnityEngine;
 
 namespace AssetLens.Reference
 {
@@ -8,40 +9,46 @@ namespace AssetLens.Reference
 		[InitializeOnLoadMethod]
 		private static async void InitializeOnLoadMethod()
 		{
-			// when there is no directory for cached object.
-			DirectoryInfo rootDirectory = new DirectoryInfo(FileSystem.ReferenceCacheDirectory);
-			if (!rootDirectory.Exists)
-			{ 
-				rootDirectory.Create();
+			string key = $"{Application.productName}.AssetLens.InitCheck";
+			if (!SessionState.GetBool(key, false))
+			{
+				// when there is no directory for cached object.
+				DirectoryInfo rootDirectory = new DirectoryInfo(FileSystem.ReferenceCacheDirectory);
+				if (!rootDirectory.Exists)
+				{ 
+					rootDirectory.Create();
 
-				// force re-indexing
-				if (ReferenceSerializer.HasLocalVersion())
-				{
-					int version = ReferenceSerializer.GetLocalVersion();
-					if (version < 100)
+					// force re-indexing
+					if (ReferenceSerializer.HasLocalVersion())
 					{
-						if (EditorUtility.DisplayDialog(
-							Localize.Inst.dialog_titleContent,
-							Localize.Inst.dialog_indexedAssetExpired,
-							Localize.Inst.dialog_enablePlugin,
-							Localize.Inst.dialog_disablePlugin))
+						int version = ReferenceSerializer.GetLocalVersion();
+						if (version < 100)
 						{
-							await AssetLensCache.CleanUpAssets();
-							await AssetLensCache.IndexAssets();
+							if (EditorUtility.DisplayDialog(
+								Localize.Inst.dialog_titleContent,
+								Localize.Inst.dialog_indexedAssetExpired,
+								Localize.Inst.dialog_enablePlugin,
+								Localize.Inst.dialog_disablePlugin))
+							{
+								await AssetLensCache.CleanUpAssets();
+								await AssetLensCache.IndexAssets();
 						
-							Setting.IsEnabled = true;
+								Setting.IsEnabled = true;
+							}
+							else
+							{
+								Setting.IsEnabled = false;
+							}
 						}
-						else
-						{
-							Setting.IsEnabled = false;
-						}
-					}
 					
-					return;
-				}
+						return;
+					}
 
-				// first indexing
-				await ReferenceDialog.OpenIndexAllAssetDialog();
+					// first indexing
+					await ReferenceDialog.OpenIndexAllAssetDialog();
+				}
+				
+				SessionState.SetBool(key, true);
 			}
 		}
 	}
