c333f8cb333dd87998ef8ee3df4807ef55af609f
seonghwan-dev <Seonghwan@pm.me>
2021-10-28 23:19:50 +0900

    multi column viewer base, component asset reference

---
+++ Editor/EditorWindow/MultiColumnReferenceViewer.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace AssetLens.Reference
+{
+    /// <summary>
+    /// Sortable Multi Column Reference Viewer Window
+    /// </summary>
+    public class MultiColumnReferenceViewer : EditorWindow
+    {
+        private const string UXML = "Packages/com.calci.assetlens/Editor/EditorWindow/MultiColumnReferenceViewer.uxml";
+        
+#if DEBUG_ASSETLENS
+        [MenuItem("Window/Asset Lens/Multi Column Reference Viewer", false, 111)]
+#endif
+        public static MultiColumnReferenceViewer ShowExample()
+        {
+            MultiColumnReferenceViewer wnd = GetWindow<MultiColumnReferenceViewer>();
+
+            wnd.titleContent = new GUIContent("Multi Column Reference Viewer");
+
+            return wnd;
+        }
+
+        public void CreateGUI()
+        {
+            // Each editor window contains a root VisualElement object
+            VisualElement root = rootVisualElement;
+
+            // Import UXML
+            VisualTreeAsset visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(UXML);
+            VisualElement editorWindowTree = visualTree.Instantiate();
+            
+            root.Add(editorWindowTree);
+        }
+    }
+}
