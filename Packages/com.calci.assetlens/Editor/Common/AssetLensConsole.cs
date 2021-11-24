f7177de91d420a87d17a6ce1ed42805cfc3c1c1e
seonghwan-dev <Seonghwan@pm.me>
2021-11-24 10:21:35 +0900

    perf: localized text update event delegate

--- Packages/com.calci.assetlens/Editor/Common/AssetLensConsole.cs
+++ Packages/com.calci.assetlens/Editor/Common/AssetLensConsole.cs
@@ -13,6 +13,7 @@ namespace AssetLens
 #if DEBUG_ASSETLENS
 			Log += Debug.Log;
 #endif
+			Verbose += Debug.Log;
 		}
 
 #if DEBUG_ASSETLENS
@@ -23,10 +24,25 @@ namespace AssetLens
 		{
 		}
 #endif
+		
+	
+#if DEBUG_ASSETLENS
+		public static Action<string> Verbose = delegate(string msg) {  };
+#else
+		[Conditional("DEBUG_ASSETLENS")]
+		public static void Verbose(string msg)
+		{
+		}
+#endif
 	}
 
 	internal static class R
 	{
+		public static string D(string msg)
+		{
+			return $"<color=#D99090FF>[AssetLens:Dev]</color> {msg}";
+		}
+		
 		public static string L(string msg)
 		{
 			return $"<color=#D99090FF>[AssetLens]</color> {msg}";
